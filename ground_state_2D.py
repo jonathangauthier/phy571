fb3fbbcdb617b77f8791158366fe035bff0c7c1a
maximeloil <maxime.loil@sfr.fr>
2018-12-03 19:53:22 +0100

    clean programme

--- ground_state_2D.py
+++ ground_state_2D.py
@@ -5,14 +5,14 @@ import scipy.linalg as lin
 from scipy.sparse import diags
 import scipy.optimize as opt
 import math
-from  scipy.misc import derivative as der
 import os.path
 from mpl_toolkits import mplot3d
+from matplotlib.colors import LogNorm
 
 src = "C:\\Users\\Maxime\\Documents\\phy571_project\\"
 os.chdir(src)
-#from background_state_1D import groundState1DCN
 
+#from ground_state_1D import groundState1DCN
 
 
 ## Definition of the Algorithm
@@ -52,57 +52,51 @@ class groundState2DCN:
         self.sigma_y = D*self.dt/(2*self.dy**2)
         
         #definition of the matrix Ax
-        diaUp = [-self.sigma_x]*(self.Jx-1)
-        diaUp[0] = 0
-        diaDown = [-self.sigma_x]*(self.Jx-1)
-        diaDown[Jx-2] = 0
-        dia = [1+2*self.sigma_x]*self.Jx
-        dia[0] = 1
-        dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Ax = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        Ax = np.zeros((3,Jx),dtype=complex)
+        Ax[0,1] = 0
+        Ax[0,2:] = -self.sigma_x
+        Ax[1,:] = 1+2*self.sigma_x
+        Ax[1,0] = 1
+        Ax[1,-1] = 1
+        Ax[2,:-2] = -self.sigma_x
+        Ax[2,-2] = 0
+        self.Ax = Ax
         
         #definition of the matrix Ay
-        diaUp = [-self.sigma_y]*(self.Jy-1)
-        diaUp[0] = 0
-        diaDown = [-self.sigma_y]*(self.Jy-1)
-        diaDown[Jy-2] = 0
-        dia = [1+2*self.sigma_y]*self.Jy
-        dia[0] = 1
-        dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Ay = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        Ay = np.zeros((3,Jy),dtype=complex)
+        Ay[0,1] = 0
+        Ay[0,2:] = -self.sigma_y
+        Ay[1,:] = 1+2*self.sigma_y
+        Ay[1,0] = 1
+        Ay[1,-1] = 1
+        Ay[2,:-2] = -self.sigma_y
+        Ay[2,-2] = 0
+        self.Ay = Ay
         
         #definition of the matrix Bx
-        diaUp = [self.sigma_x]*(self.Jx-1)
+        diaUp = self.sigma_x*np.ones(self.Jx-1,dtype=complex)
         diaUp[0] = 0
-        diaDown = [self.sigma_x]*(self.Jx-1)
-        diaDown[Jx-2] = 0
-        dia = [1-2*self.sigma_x]*self.Jx
+        diaDown = self.sigma_x*np.ones(self.Jx-1,dtype=complex)
+        diaDown[self.Jx-2] = 0
+        dia = (1-2*self.sigma_x)*np.ones(self.Jx,dtype=complex)
         dia[0] = 1
         dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Bx = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        self.Bx = diags([diaUp,dia,diaDown], [1,0,-1], (self.Jx, self.Jx), format='csr')
         
         #definition of the matrix By
-        diaUp = [self.sigma_y]*(self.Jy-1)
+        diaUp = self.sigma_y*np.ones(self.Jy-1,dtype=complex)
         diaUp[0] = 0
-        diaDown = [self.sigma_y]*(self.Jy-1)
-        diaDown[Jy-2] = 0
-        dia = [1-2*self.sigma_y]*self.Jy
+        diaDown = self.sigma_y*np.ones(self.Jy-1,dtype=complex)
+        diaDown[self.Jy-2] = 0
+        dia = (1-2*self.sigma_y)*np.ones(self.Jy,dtype=complex)
         dia[0] = 1
         dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.By = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        self.By = diags([diaUp,dia,diaDown], [1,0,-1], (self.Jy, self.Jy), format='csr')
         
         #definition of the initial condition
         self.U = np.array((np.vectorize(u0))(self.X,self.Y),dtype=complex)
         self.oldU = np.zeros_like(self.U, dtype=complex)
         self.isFirstStep = True
-        
-        #storage of Ax^-1 and Ay^-1
-        self.Ax_inv = lin.inv(self.Ax)
-        self.Ay_inv = lin.inv(self.Ay)
     
     def change_a(self,a):
         self.a = a
@@ -112,73 +106,67 @@ class groundState2DCN:
         self.sigma_y = D*self.dt/(2*self.dy**2)
         
         #redefinition of the matrix Ax
-        diaUp = [-self.sigma_x]*(self.Jx-1)
-        diaUp[0] = 0
-        diaDown = [-self.sigma_x]*(self.Jx-1)
-        diaDown[self.Jx-2] = 0
-        dia = [1+2*self.sigma_x]*self.Jx
-        dia[0] = 1
-        dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Ax = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        Ax = np.zeros((3,self.Jx),dtype=complex)
+        Ax[0,1] = 0
+        Ax[0,2:] = -self.sigma_x
+        Ax[1,:] = 1+2*self.sigma_x
+        Ax[1,0] = 1
+        Ax[1,-1] = 1
+        Ax[2,:-2] = -self.sigma_x
+        Ax[2,-2] = 0
+        self.Ax = Ax
         
         #redefinition of the matrix Ay
-        diaUp = [-self.sigma_y]*(self.Jy-1)
-        diaUp[0] = 0
-        diaDown = [-self.sigma_y]*(self.Jy-1)
-        diaDown[self.Jy-2] = 0
-        dia = [1+2*self.sigma_y]*self.Jy
-        dia[0] = 1
-        dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Ay = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        Ay = np.zeros((3,self.Jy),dtype=complex)
+        Ay[0,1] = 0
+        Ay[0,2:] = -self.sigma_y
+        Ay[1,:] = 1+2*self.sigma_y
+        Ay[1,0] = 1
+        Ay[1,-1] = 1
+        Ay[2,:-2] = -self.sigma_y
+        Ay[2,-2] = 0
+        self.Ay = Ay
         
         #redefinition of the matrix Bx
-        diaUp = [self.sigma_x]*(self.Jx-1)
+        diaUp = self.sigma_x*np.ones(self.Jx-1,dtype=complex)
         diaUp[0] = 0
-        diaDown = [self.sigma_x]*(self.Jx-1)
+        diaDown = self.sigma_x*np.ones(self.Jx-1,dtype=complex)
         diaDown[self.Jx-2] = 0
-        dia = [1-2*self.sigma_x]*self.Jx
+        dia = (1-2*self.sigma_x)*np.ones(self.Jx,dtype=complex)
         dia[0] = 1
         dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.Bx = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
+        self.Bx = diags([diaUp,dia,diaDown], [1,0,-1], (self.Jx, self.Jx), format='csr')
         
         #redefinition of the matrix By
-        diaUp = [self.sigma_y]*(self.Jy-1)
+        diaUp = self.sigma_y*np.ones(self.Jy-1,dtype=complex)
         diaUp[0] = 0
-        diaDown = [self.sigma_y]*(self.Jy-1)
+        diaDown = self.sigma_y*np.ones(self.Jy-1,dtype=complex)
         diaDown[self.Jy-2] = 0
-        dia = [1-2*self.sigma_y]*self.Jy
+        dia = (1-2*self.sigma_y)*np.ones(self.Jy,dtype=complex)
         dia[0] = 1
         dia[-1] = 1
-        diagonals = [dia, diaUp, diaDown]
-        self.By = np.array(diags(diagonals, [0, 1, -1]).toarray(),dtype=complex)
-        
-        #redefinition of Ax^-1 and Ay^-1
-        self.Ax_inv = lin.inv(self.Ax)
-        self.Ay_inv = lin.inv(self.Ay)
+        self.By = diags([diaUp,dia,diaDown], [1,0,-1], (self.Jy, self.Jy), format='csr')
         
     def step(self):
         """Calculate the next step in the Crank-Nicholson algorithm"""
         #for the first step, we calculate only with the previous state
         if self.isFirstStep:
-            F = (np.vectorize(self.pot.f))(self.x, self.y, self.U)
+            F = (self.pot.f)(self.X, self.Y, self.U)
             self.isFirstStep = False
         #else, we calculate with the previous step and the one before to keep the second order accuracy
         else:
-            F = 3/2*(np.vectorize(self.pot.f))(self.x, self.y, self.U)-1/2*(np.vectorize(self.pot.f))(self.x, self.y, self.oldU)
+            F = 3/2*(self.pot.f)(self.X, self.Y, self.U)-1/2*(self.pot.f)(self.X, self.Y, self.oldU)
         self.oldU = np.copy(self.U)
         #Propagation in x
-        C = np.dot(self.Bx,self.U)+0.5*self.dt*F
+        C = self.Bx.dot(self.U)+0.5*self.dt*F
         C[0,:] = 0
         C[-1,:] = 0
-        self.U = np.dot(self.Ax_inv,C)
+        self.U = lin.solve_banded((1,1), self.Ax, C, check_finite=False)
         #Propagation in y
-        C = np.dot(self.By,self.U.T)+0.5*self.dt*F
+        C = (self.By.dot(self.U.T))+0.5*self.dt*F
         C[0,:] = 0
         C[-1,:] = 0
-        self.U = np.dot(self.Ay_inv,C).T
+        self.U = lin.solve_banded((1,1), self.Ay, C, check_finite=False).T
         
     def renorm(self,vortex=False):
         """Renormalize the current solution"""
@@ -195,8 +183,8 @@ class Potential:
         self.wx = wx
         self.wy = wy
         self.Ng = Ng
-        self.m = 1
-        self.hbar = 1
+        self.m = m
+        self.hbar = hbar
         
         def V(x,y):
             """Potential of the BEC"""
@@ -218,12 +206,12 @@ class Potential:
 
 xMax = 10
 dtau = 0.02
-J = 51 #number of spatial points
+J = 201 #number of spatial points
 hbar = 1
 m = 1
 D = 1j*hbar/(2*m)
 w = 0.1 #paramater of harmonic oscillator
-Ng = 1000*0.05/10
+Ng = 5
 a=-1j #imaginary time
 
 
@@ -258,7 +246,7 @@ def harmonic_state_2D(nx,ny):
     return E, psi
 
 
-def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isRenormed=True,shape='contours',saveMP4=False,plotAnalytical=True,vortex=False,xv=0,yv=0):
+def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isRenormed=True,shape='contours',saveMP4=False,plotAnalytical=True,vortex=False):
     """Print the imaginary time evolution of the BEC to the ground state
             number_of_steps_per_frame: how many steps are calculated for each frame
             potential_size_factor: the potential is divided by this value to fit in the window
@@ -276,10 +264,6 @@ def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isR
     
     E, psi = harmonic_state_2D(0,0)
     
-    """
-    xv_coord = int((xv-gS.xMin)//gS.dx + ((xv-gS.xMin)%gS.dx>0.5))+1
-    yv_coord = int((yv-gS.yMin)//gS.dy + ((yv-gS.yMin)%gS.dy>0.5))+1
-    """
     
     def make_frame(k):
         if isRenormed:
@@ -291,13 +275,13 @@ def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isR
         ax.clear()
         
         if shape == 'surface':
-            wave_function = ax.plot_surface(gS.X,gS.Y,np.abs(gS.U)**2, label="$Wave \enspace function$",cmap='winter',alpha=opacity)
+            density_proba = ax.plot_surface(gS.X,gS.Y,np.abs(gS.U)**2, label="$Density \enspace probability$",cmap='winter',alpha=opacity)
             if plotAnalytical:
                 analytical = ax.plot_surface(gS.X,gS.Y,np.abs(psi)**2,rstride=1,label="$Analytical \enspace solution \enspace level \enspace 0$",cmap='autumn',alpha=0.6)
             #potential = ax.plot_surface(gS.X,gS.Y,gS.pot.V(gS.X,gS.Y)/potential_size_factor,label="$Potential \enspace V$",color=(0.5,0,0,0.5))
             #effective_potential = ax.plot_surface(gS.X,gS.Y,Veff_current,label="$Effective \enspace potential \enspace Veff$")
         elif shape == 'contours':
-            wave_function = ax.contour3D(gS.X, gS.Y, np.abs(gS.U)**2, 10, cmap='Blues')
+            density_proba = ax.contour3D(gS.X, gS.Y, np.abs(gS.U)**2, 10, cmap='Blues')
             if plotAnalytical:
                 analytical = ax.contour3D(gS.X,gS.Y,np.abs(psi)**2,10,label="$Analytical \enspace solution \enspace level \enspace 0$",cmap='Reds')
 
@@ -310,13 +294,11 @@ def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isR
             if isRenormed:
                 gS.renorm(vortex)
             gS.step()
-            if vortex:
-                gS.U[xv_coord,yv_coord]=0
 
-        return wave_function,
+        return density_proba,
     
 
-    ani = animation.FuncAnimation(fig, make_frame, frames = 70, interval = 20, blit=False)
+    ani = animation.FuncAnimation(fig, make_frame, frames = 70, interval = number_of_steps_per_frame*10, blit=False)
 
     if saveMP4:
         Writer = animation.writers['ffmpeg']
@@ -326,9 +308,39 @@ def evolution_anim(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isR
         ani.save(src+'CN2D.mp4', writer=writer)
     else:
         plt.title("$Evolution \enspace process \enspace to \enspace get \enspace the \enspace ground \enspace state$")
-        #plt.legend()
         plt.show()
 
+def evolution_anim_color(number_of_steps_per_frame, potential_size_factor=50,a=-1j,isRenormed=True,shape='contours',saveMP4=False,plotAnalytical=True,vortex=False,xv=0,yv=0):
+    """Print the imaginary time evolution of the BEC to the ground state
+            number_of_steps_per_frame: how many steps are calculated for each frame
+            potential_size_factor: the potential is divided by this value to fit in the window
+    """
+    gS.change_a(a)
+    
+    fig,ax = plt.subplots()
+
+    mesh = ax.pcolormesh(gS.X,gS.Y,np.zeros_like(gS.X), norm=LogNorm(vmin=0.0001,vmax=1))
+    plt.colorbar(mesh,ax=ax)
+
+    
+    def make_frame(k):
+        if isRenormed:
+            gS.renorm(vortex)
+        
+        C = (np.abs(gS.U)**2)[:-1,:-1]
+        mesh.set_array(C.ravel())
+
+        ax.set_title("{:1.1e}".format(np.sum((np.abs(gS.oldU-gS.U))**2)))
+            
+        for i in range(number_of_steps_per_frame):
+            if isRenormed:
+                gS.renorm(vortex)
+            gS.step()
+            
+        return mesh,
+    
+    ani = animation.FuncAnimation(fig, make_frame, blit=False)
+    plt.show()
 
 def get_energy(f,psi):
     """Get the energy of a state, and give the result of the hamiltonian over the state"""
@@ -342,7 +354,7 @@ def get_energy(f,psi):
     return E,H_psi
 
 
-def get_ground(gS, threshold=1e-7):
+def get_ground(gS, threshold=1e-8):
     gS.renorm()
     gS.step()
     while np.sum((np.abs(gS.oldU-gS.U))**2)>threshold:
@@ -352,154 +364,56 @@ def get_ground(gS, threshold=1e-7):
     return gS.U
     
     
-
-def get_energies(Ng_min,Ng_max,Ng_nbr=11):
-    Ng_array = np.linspace(Ng_min,Ng_max,Ng_nbr)
-    E_array = np.zeros_like(Ng_array)
-    for i, Ng in enumerate(Ng_array):
-        def V(x):
-            """Potential of the BEC"""
-            return 0.5*m*(w*x)**2
-            
-        def Veff(x,u):
-            """Effective potential of the BEC"""
-            return V(x)+ Ng*np.abs(u)**2
-            
-        def f(x,u):
-            return 1/(1j*hbar)*Veff(x,u)*u
-            
-        gS = groundState1DCN(-xMax,xMax,dtau,J,D,f,u0)
-        
-        bkgd_wave_func = get_ground(gS)
-        E,H_psi = get_energy(f,bkgd_wave_func)
-        E_array[i] = E
-        print(i)
-        
-    np.savetxt(src+"data\\"+"E_array.txt",E_array)
-
-def get_standard_deviation(psi):
-    return (np.sum(np.conjugate(psi)*gS.x**2*psi)*gS.dx-(np.sum(np.conjugate(psi)*gS.x*psi)*gS.dx)**2)**0.5
-
-def get_standard_deviations(Ng_min,Ng_max,Ng_nbr=11):
-    Ng_array = np.linspace(Ng_min,Ng_max,Ng_nbr)
-    std_array = np.zeros_like(Ng_array)
-    for i, Ng in enumerate(Ng_array):
-        def V(x):
-            """Potential of the BEC"""
-            return 0.5*m*(w*x)**2
-            
-        def Veff(x,u):
-            """Effective potential of the BEC"""
-            return V(x)+ Ng*np.abs(u)**2
-            
-        def f(x,u):
-            return 1/(1j*hbar)*Veff(x,u)*u
-            
-        gS = groundState1DCN(-xMax,xMax,tauMax,J,N,D,f,u0)
-        
-        bkgd_wave_func = get_ground(gS)
-        std = get_standard_deviation(bkgd_wave_func)
-        std_array[i] = std
-        print(i)
-       
-    np.savetxt(src+"data\\"+"std_array.txt",std_array)
-
-
-def plot_energies(Ng_min,Ng_max,Ng_nbr=11):
-    Ng_array = np.linspace(Ng_min,Ng_max,Ng_nbr)
-    E_array = np.loadtxt(src+"data\\"+"E_array.txt",dtype=complex)
-    plt.plot(Ng_array,E_array,'.')
-    E_harm,_ = harmonic_state(0)
-    plt.hlines(E_harm,Ng_min,Ng_max,label="Without non linearity")
-    plt.xlabel("$Ng$")
-    plt.ylabel("$E$")
-    plt.title("$Ground \enspace state \enspace energies \enspace for \enspace different \enspace values \enspace of \enspace Ng$")
-    plt.legend(loc="lower right")
-    plt.show()
-    
-    
-def plot_standard_deviations(Ng_min,Ng_max,Ng_nbr=11):
-    Ng_array = np.linspace(Ng_min,Ng_max,Ng_nbr)
-    std_array = np.loadtxt(src+"data\\"+"std_array.txt",dtype=complex)
-    plt.plot(Ng_array,std_array,'.')
-    E_harm,psi_harm = harmonic_state(0)
-    plt.hlines(get_standard_deviation(psi_harm),Ng_min,Ng_max,label="Without non linearity")
-    plt.xlabel("$Ng$")
-    plt.ylabel("$\sigma$")
-    plt.title("$Ground \enspace state \enspace standard \enspace deviations \enspace for \enspace different \enspace values \enspace of \enspace Ng$")
-    plt.legend(loc="upper right")
-    plt.show()
-    
-
 def save_ground(gS,overwrite=False):
-    name_func = "grd_state_"+str(int(gS.xMax))+"_"+str(int(gS.tauMax))+"_"+str(int(gS.J))+"_"+str(int(gS.N))+\
-                "_"+"{:1.1e}".format(gS.pot.w)+"_"+str(int(200*gS.pot.Ng))
+    name_func = "grd_state_2D_"+\
+                str(int(gS.xMax))+"_"+\
+                "{:1.1e}".format(gS.dtau)+"_"+\
+                str(int(gS.Jx))+"_"+\
+                "{:1.1e}".format(gS.pot.wx)+"_"+\
+                "{:1.1e}".format(gS.pot.Ng)
                 
     if overwrite or not os.path.isfile(src+"data\\"+name_func):
         grd_wave_func = get_ground(gS)
-        np.savetxt(src+"data\\"+name_func,grd_wave_func)
-
+        np.savetxt(src+"data\\"+name_func+".txt",grd_wave_func)
 
 def load_ground(gS):
-    name_func = "grd_state_"+str(int(gS.xMax))+"_"+str(int(gS.tauMax))+"_"+str(int(gS.J))+"_"+str(int(gS.N))+\
-                "_"+"{:1.1e}".format(gS.pot.w)+"_"+str(int(200*gS.pot.Ng))
-    gS.U = np.loadtxt(src+"data\\"+name_func,dtype=complex)
+    name_func = "grd_state_2D_"+\
+                str(int(gS.xMax))+"_"+\
+                "{:1.1e}".format(gS.dtau)+"_"+\
+                str(int(gS.Jx))+"_"+\
+                "{:1.1e}".format(gS.pot.wx)+"_"+\
+                "{:1.1e}".format(gS.pot.Ng)
+    gS.U = np.loadtxt(src+"data\\"+name_func+".txt",dtype=complex)
     
     
 ## Evolution
 
 #Animation of the evolution
 
-evolution_anim(20,10,a=-1j,isRenormed=True,shape='surface',plotAnalytical=False,vortex=True)
-#evolution_anim(20,10,a=1,isRenormed=False,shape='surface',plotAnalytical=False,vortex=False)
 
-#Check with the ground state of the harmonic oscillator
-"""
-n = 0
-E_theo, psi = harmonic_state(n)
-E_calc,H_psi = get_energy(lambda x,u: 1/(1j*hbar)*V(x)*u,psi)
-
-plt.plot(gS.x,np.abs(psi)**2,label="harmonic eigen state")
-plt.plot(gS.x,[E_theo]*J,label="theoretical energy")
-plt.plot(gS.x,[E_calc]*J,label="calculated energy")
-plt.legend()
-plt.show()
-"""
+#evolution_anim_color(10,10,a=-1j,isRenormed=True,vortex=False)
+#evolution_anim_color(10,10,a=1,isRenormed=False,vortex=True)
 
 
-#save ground
-"""
-bkgd_wave_func = get_ground()
-np.savetxt(src+"data\\"+"bkgd_wave_func.txt",bkgd_wave_func)
-"""
+#evolution_anim(10,10,a=-1j,isRenormed=True,shape='surface',plotAnalytical=False,vortex=True)
+#evolution_anim(10,10,a=1,isRenormed=False,shape='surface',plotAnalytical=False,vortex=False)
 
-#load ground
-"""
-bkgd_wave_func = np.loadtxt(src+"data\\"+"bkgd_wave_func.txt",dtype=complex)
-plt.plot(np.abs(a)**2)
-plt.plot(np.abs(bkgd_wave_func)**2)
-plt.show()
-"""
+
+
+#evolution_anim(20,10,a=1,isRenormed=False)
 
 """
-#get_energies(-2000*0.05/10,2000*0.05/10)
-plot_energies(-2000*0.05/10,2000*0.05/10)
+xv_coord = int((xv-gS.xMin)//gS.dx + ((xv-gS.xMin)%gS.dx>0.5))+1
+yv_coord = int((yv-gS.yMin)//gS.dy + ((yv-gS.yMin)%gS.dy>0.5))+1
 """
 
-"""
-#get_standard_deviations(-2000*0.05/10,2000*0.05/10)
-plot_standard_deviations(-2000*0.05/10,2000*0.05/10)
-"""
 
-"""
 save_ground(gS)
 load_ground(gS)
 
-grd_wave_func = gS.U
 
 
-plt.plot(gS.x,np.abs(grd_wave_func)**2)
-plt.show()
-"""
 
-#evolution_anim(20,10,a=1,isRenormed=False)
+
+
+
